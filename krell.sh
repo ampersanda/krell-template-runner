ad5c8869818557798726c44ec8abf873bd839869
Mochamad Lucky Pradana <ampersanda.clj@icloud.com>
2020-05-01 21:48:43 +0700

    feat: add babashka installer

--- krell.sh
+++ krell.sh
@@ -3,6 +3,13 @@
 rm -rf krell-template-runner
 git clone https://github.com/ampersanda/krell-template-runner
 cd krell-template-runner
+
+if ! type bb > /dev/null; then
+  echo "👉 Babashka is not installed. Installing babashka..."
+  bash <(curl -s https://raw.githubusercontent.com/borkdude/babashka/master/install)
+  echo "👉 Babashka installed."
+fi
+
 bb runner.clj $1
 mv $1 ..
 cd ..
